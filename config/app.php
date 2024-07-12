23e92dec28e0f9423574345b45c806d09ce9f526
pepetgoodboy <iqbaltw127@gmail.com>
2024-07-12 14:15:45 +0700

    fix vite

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://mynotes-production-948b.up.railway.app'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
